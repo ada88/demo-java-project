5d12476dd1961c2e5d9c2baeb9c8225b07f3bda4
Ali Doğan Aydın <alidoganaydin88@gmail.com>
2024-07-31 15:18:30 +0300

    Update DemoJavaProjectApplication.java

--- src/main/java/com/example/demo/DemoJavaProjectApplication.java
+++ src/main/java/com/example/demo/DemoJavaProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoJavaProjectApplication {
+//public class DemoJavaProjectApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(DemoJavaProjectApplication.class, args);
