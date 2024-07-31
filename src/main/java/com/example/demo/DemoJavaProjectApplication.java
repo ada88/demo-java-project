d8088f8218d09db80082c97b5cfc2d615b0572c1
ada88 <alidoganaydin88@gmail.com>
2024-07-31 15:17:16 +0300

    Update DemoJavaProjectApplication.java

--- src/main/java/com/example/demo/DemoJavaProjectApplication.java
+++ src/main/java/com/example/demo/DemoJavaProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJavaProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoJavaProjectApplication.class, args);
+		//SpringApplication.run(DemoJavaProjectApplication.class, args);
 		//Test
 	}
 
